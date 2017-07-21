dd90909d982bb5f436b505f816b784765252f311
Carter Green <gree@belvederetrading.com>
2017-07-21 13:18:15 -0500

    Simple betting carnival game

--- betting.py
+++ betting.py
@@ -1,16 +1,35 @@
-from __future__ import print_function
+from __future__ import print_function, division
 import sys
 from random import randint, seed
+from enum import Enum
+import numpy as np
 range = xrange
 
 
+def fix(over_under):
+    """Create a skewed seed distribution."""
+    seed_range = np.arange(1, 101)
+    # Make each of the last 41 elements 5x more likely
+    low = [1.0] * (len(seed_range) - 40)
+    high = [5.0] * 40
+    prob_dist = np.array(low + high if over_under == "over" else high + low)
+    # Normalising to 1.0
+    prob_dist = np.divide(prob_dist, np.sum(prob_dist))
+    number = np.random.choice(seed_range, 1, p=prob_dist)
+    return int(number)
+
 def calc_pnl(style, bet, diff):
+    """Calculate profit and loss for a given bet, bet style and difference between
+    the line and the random number."""
     if style == "binary":
         return bet
     else:
         return bet * abs(diff)
 
 def betting(times, bet, over_under, style):
+    if over_under not in ("over", "under"):
+        raise ValueError("Choose between betting over or under.")
+    seed(fix(over_under))
     over = 6
     under = 5
     pnl = 0
