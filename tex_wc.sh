dd914ec8b5423c1050ca86e7d22c5dd8ff1aaf33
Carter Green <cartergr@usc.edu>
2017-09-14 09:57:42 -0700

    Moved bash scripts to dotfiles

--- tex_wc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# wc prints the number of newlines, word count and character count
-# awk -F" " defines whitespace as a field separator
-# print $2 prints the second field: the word count
-FILE="$1"
-WC=$(detex "$FILE" | wc | awk -F" " '{print $2}')
-echo "$FILE contains $WC words"
