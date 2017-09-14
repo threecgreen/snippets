4885b30018adf4dbb9591cabe7cb4e66f92b2460
Carter Green <cartergr@usc.edu>
2017-09-14 01:04:52 -0700

    Add shebang

--- conda_jupyter.sh
+++ conda_jupyter.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Script for installing jupyter kernel in a conda environment
 # Environment kernel will be "Python {Python Version Number (2 | 3)} ({environment name})"
 source activate "$1"
